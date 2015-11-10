cd5b8c14f77d1f1f471ffba8c53c6a3134ac971d
Theo Anderson <theoanderson3@gmail.com>
2015-11-10 14:37:39 -0800

    yo

--- application/config/database.php
+++ application/config/database.php
@@ -18,9 +18,9 @@ else
 	$db['default']['database'] = '';
 }
 
-$db['default']['dbdriver'] = 'sqlsrv';
+$db['default']['dbdriver'] = 'pdo';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = FALSE;
+$db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
