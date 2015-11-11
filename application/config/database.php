5854f23f0a86bb5609772320aa5a04112e109960
Theo Anderson <theoanderson3@gmail.com>
2015-11-10 16:12:20 -0800

    changed

--- application/config/database.php
+++ application/config/database.php
@@ -5,14 +5,14 @@ $active_record = TRUE;
 
 if(ENVIRONMENT == 'production')
 {
-	$db['default']['hostname'] = 'ahmxn6q9c3.discussionboard.windows.net,1433';
+	$db['default']['hostname'] = 'ahmxn6q9c3.database.windows.net,1433';
 	$db['default']['username'] = 'theo';
 	$db['default']['password'] = 'Anderson1993!';
 	$db['default']['database'] = 'discussionboard';
 }
 else
 {
-	$db['default']['hostname'] = 'ahmxn6q9c3.discussionboard.windows.net,1433';
+	$db['default']['hostname'] = 'ahmxn6q9c3.database.windows.net,1433';
 	$db['default']['username'] = 'theo';
 	$db['default']['password'] = 'Anderson1993!';
 	$db['default']['database'] = 'discussionboard';
