a196ad6599005ce79c3d12cc27ef6daca68f5f83
Theo Anderson <theoanderson3@gmail.com>
2015-11-11 09:55:00 -0800

    push

--- application/config/database.php
+++ application/config/database.php
@@ -17,9 +17,9 @@ else
 	$db['default']['password'] = '';
 	$db['default']['database'] = '';
 }
-$db['default']['dbdriver'] = 'pdo';
+$db['default']['dbdriver'] = 'sqlsrv';
 $db['default']['dbprefix'] = '';
-$db['default']['pconnect'] = FALSE;
+$db['default']['pconnect'] = TRUE;
 $db['default']['db_debug'] = TRUE;
 $db['default']['cache_on'] = FALSE;
 $db['default']['cachedir'] = '';
