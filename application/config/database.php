0e844558c4daf099dec3b4d5b5cc78739e0535fb
Theo Anderson <theoanderson3@gmail.com>
2015-11-11 09:50:25 -0800

    push

--- application/config/database.php
+++ application/config/database.php
@@ -12,10 +12,10 @@ if(ENVIRONMENT == 'production')
 }
 else
 {
-	$db['default']['hostname'] = 'ahmxn6q9c3.database.windows.net,1433';
-	$db['default']['username'] = 'theo';
-	$db['default']['password'] = 'Anderson1993!';
-	$db['default']['database'] = 'discussionboard';
+	$db['default']['hostname'] = '';
+	$db['default']['username'] = '';
+	$db['default']['password'] = '';
+	$db['default']['database'] = '';
 }
 $db['default']['dbdriver'] = 'sqlsrv';
 $db['default']['dbprefix'] = '';
