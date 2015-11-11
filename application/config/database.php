049922cd0abd43236c03b202abd58363ebe7061b
Theo Anderson <theoanderson3@gmail.com>
2015-11-11 10:13:48 -0800

    yo

--- application/config/database.php
+++ application/config/database.php
@@ -17,7 +17,7 @@ else
 	$db['default']['password'] = 'Anderson1993!';
 	$db['default']['database'] = '';
 }
-$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbdriver'] = 'sqlsrv';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
