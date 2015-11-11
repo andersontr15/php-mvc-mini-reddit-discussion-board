3b956d6a0bcda5e5fa1d8ac2972dfb805203c4fb
Theo Anderson <theoanderson3@gmail.com>
2015-11-11 10:54:27 -0800

    yo

--- application/config/database.php
+++ application/config/database.php
@@ -5,17 +5,17 @@ $active_record = TRUE;
 
 if(ENVIRONMENT == 'production')
 {
-	$db['default']['hostname'] = 'us-cdbr-azure-west-c.cloudapp.net';
-	$db['default']['username'] = 'b98de423f68884';
-	$db['default']['password'] = '016a22d4';
-	$db['default']['database'] = 'discussionboard';
+	$db['default']['hostname'] = 'ahmxn6q9c3.database.windows.net,1433';
+	$db['default']['username'] = 'theo';
+	$db['default']['password'] = 'Anderson1993!';
+	$db['default']['database'] = '';
 }
 else
 {
-	$db['default']['hostname'] = 'us-cdbr-azure-west-c.cloudapp.net';
-	$db['default']['username'] = 'b98de423f68884';
-	$db['default']['password'] = '016a22d4';
-	$db['default']['database'] = 'discussionboard';
+	$db['default']['hostname'] = 'ahmxn6q9c3.database.windows.net,1433';
+	$db['default']['username'] = 'theo';
+	$db['default']['password'] = 'Anderson1993!';
+	$db['default']['database'] = '';
 }
 $db['default']['dbdriver'] = 'sqlsrv';
 $db['default']['dbprefix'] = '';
