4f9cea1c43ba37f9abc79802aa92cd66dcff4e63
Theo Anderson <theoanderson3@gmail.com>
2015-11-12 19:25:26 -0800

    yo

--- application/config/database.php
+++ application/config/database.php
@@ -17,7 +17,7 @@ else
 	$db['default']['password'] = 'password';
 	$db['default']['database'] = 'discussionboard';
 }
-$db['default']['dbdriver'] = 'sqlsrv';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
