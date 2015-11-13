cebf426acc87a9884262d667cf8b6200c9aeccda
Theo Anderson <theoanderson3@gmail.com>
2015-11-12 20:35:45 -0800

    sdfsdf

--- application/config/database.php
+++ application/config/database.php
@@ -17,7 +17,7 @@ else
 	$db['default']['password'] = '3b0ae80a';
 	$db['default']['database'] = 'discussionboard';
 }
-$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
