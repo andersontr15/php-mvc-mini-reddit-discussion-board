473e541e332f385c574b325a19b524a3946004e0
Theo Anderson <theoanderson3@gmail.com>
2015-11-12 20:48:51 -0800

    db update

--- application/config/database.php
+++ application/config/database.php
@@ -6,18 +6,18 @@ $active_record = TRUE;
 if(ENVIRONMENT == 'production')
 {
 	$db['default']['hostname'] = 'us-cdbr-azure-west-c.cloudapp.net';
-	$db['default']['username'] = 'theo';
-	$db['default']['password'] = 'Anderson1993!';
-	$db['default']['database'] = 'discussionboard';
+	$db['default']['username'] = 'username';
+	$db['default']['password'] = 'password';
+	$db['default']['database'] = 'ahmxn6q9c3.database.windows.net';
 }
 else
 {
 	$db['default']['hostname'] = 'us-cdbr-azure-west-c.cloudapp.net';
-	$db['default']['username'] = 'theo';
-	$db['default']['password'] = 'Anderson1993!';
-	$db['default']['database'] = 'discussionboard';
+	$db['default']['username'] = 'username';
+	$db['default']['password'] = 'password';
+	$db['default']['database'] = 'ahmxn6q9c3.database.windows.net';
 }
-$db['default']['dbdriver'] = 'mysql';
+$db['default']['dbdriver'] = 'mysqli';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
