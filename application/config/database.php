a4a78ed92366cd12cbd89e50af4d7e57fcb649cd
Theo Anderson <theoanderson3@gmail.com>
2015-11-12 20:49:18 -0800

    hello

--- application/config/database.php
+++ application/config/database.php
@@ -17,7 +17,7 @@ else
 	$db['default']['password'] = 'password';
 	$db['default']['database'] = 'ahmxn6q9c3.database.windows.net';
 }
-$db['default']['dbdriver'] = 'mysqli';
+$db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = '';
 $db['default']['pconnect'] = FALSE;
 $db['default']['db_debug'] = TRUE;
